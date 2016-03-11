db79aa7997dc6d0d0fa790b9a120ab95b2e33339
Marc Wäckerlin <marc@waeckerlin.org>
2016-03-11 12:33:28 +0100

    fixed bugs

---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if test -e /firstrun; then
+    if test -z "${PASSWORD}"; then
+        echo "**** ERROR: set a password, use -e PASSWORD=YourPassWord" 1>&2
+        exit 1
+    fi
+    echo "Setting Up Dokuwiki ..."
+    debconf-set-selections <<EOF
+dokuwiki dokuwiki/wiki/superuser string ${ADMIN}
+dokuwiki dokuwiki/wiki/password password ${PASSWORD}
+dokuwiki dokuwiki/wiki/confirm password ${PASSWORD}
+dokuwiki dokuwiki/system/documentroot string ${ROOT}
+dokuwiki dokuwiki/system/configure-webserver multiselect apache2
+dokuwiki dokuwiki/system/writeplugins boolean true
+dokuwiki dokuwiki/system/writeconf boolean true
+dokuwiki dokuwiki/system/accessible select global
+dokuwiki dokuwiki/system/purgepages boolean false
+dokuwiki dokuwiki/system/restart-webserver boolean false
+dokuwiki dokuwiki/wiki/license select gnufdl
+dokuwiki dokuwiki/wiki/policy select public
+dokuwiki dokuwiki/wiki/acl boolean true
+EOF
+    DEBIAN_FRONTEND=noninteractive apt-get install -y dokuwiki
+    rm /firstrun
+    echo "Dokuwiki Configured."
+fi
+
+echo "see: http://localhost${ROOT}"
+apache2ctl -DFOREGROUND
